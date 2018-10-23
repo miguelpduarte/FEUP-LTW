ade8dbc7ce66e8d166650a16db3100399cdec580
Guilherme Silva <guilhermejosesilva@gmail.com>
2018-10-23 12:31:35 +0100

    Initial database

---
+++ database/stories.sql
@@ -0,0 +1,44 @@
+CREATE TABLE users (
+    user_id INTEGER PRIMARY KEY,
+    points INTEGER NOT NULL,
+    username VARCHAR UNIQUE NOT NULL,
+    password VARCHAR NOT NULL,
+    name VARCHAR NOT NULL
+);
+
+CREATE TABLE stories (
+    story_id INTEGER PRIMARY KEY,
+    author INTEGER REFERENCES users NOT NULL,
+    content VARCHAR NOT NULL,
+    channel INTEGER REFERENCES channels NOT NULL
+);
+
+CREATE TABLE channels (
+    channel_id INTEGER PRIMARY KEY,
+    name VARCHAR UNIQUE NOT NULL
+);
+
+CREATE TABLE comments (
+    comment_id INTEGER PRIMARY KEY,
+    content VARCHAR NOT NULL,
+    story REFERENCES stories,
+    parent_comment REFERENCES comments,
+    CHECK(
+            (story is NULL 
+            OR parent_comment is NULL) 
+        AND 
+            NOT(story is NULL 
+                AND parent_comment is NULL))
+);
+
+CREATE TABLE commentVotes(
+    user_id INTEGER REFERENCES users NOT NULL,
+    comment_id INTEGER REFERENCES comments NOT NULL,
+    is_up INTEGER CHECK(is_up = 1 OR is_up = 0)
+);
+
+CREATE TABLE storyVotes(
+    user_id INTEGER REFERENCES users NOT NULL,
+    story_id INTEGER REFERENCES stories NOT NULL,
+    is_up INTEGER CHECK(is_up = 1 OR is_up =is 0)
+);
