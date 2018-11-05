d443b4e43435ad8285aa3998e76b19544122cf71
miguelpduarte <miguelpduarte98@gmail.com>
2018-11-05 19:21:33 +0000

    Fixed some populating mistakes with newlines and expanded content so that the card expansion is notiseable

--- src/database/populate.sql
+++ src/database/populate.sql
@@ -9,10 +9,21 @@ INSERT INTO channels values(2, 'definitelynotreddit');
 -- stories
 INSERT INTO stories(story_id, author, title, content, channel) VALUES(1, 1, 'This is a very existing title', 'This is a very existing story, not a test at all', 1);
 INSERT INTO stories(story_id, author, title, content, channel) VALUES(2, 2, 'This is another very existing title', 'This is another story that does exist and is not a test at all', 2);
-INSERT INTO stories(story_id, author, title, content, channel) VALUES(3, 2, 'Marking story!', '# Did you know that\n\n## This is\n\n*Something* cool __here__?', 2);
+INSERT INTO stories(story_id, author, title, content, channel) VALUES(3, 2, 'Marking story!',
+'# Did you know that
+## This is
+*Something* cool __here__?
+This now has a lot of content that goes on and on and on and on and on and on and on and on and on and on and on and on and on
+
+Oh look! A penguin! ![penguin](https://cdn.pixabay.com/photo/2013/07/13/11/44/penguin-158551__340.png)',
+2);
 
 -- comments
-INSERT INTO comments VALUES(1, 'This comments has *markdown*!\n\n> This is some sort of a quote\n\nAnd this is not!', 2, 3, NULL);
+INSERT INTO comments VALUES(1,
+'This comments has *markdown*!
+> This is some sort of a quote
+And this is not!',
+2, 3, NULL);
 INSERT INTO comments VALUES(2, 'This is a sub-level _reply_ to the **parent** comment!', 1, NULL, 1);
 
 -- comment votes
