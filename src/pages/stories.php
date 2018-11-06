f9369ede546f620b00fc9322775f5b1b9185936d
miguelpduarte <miguelpduarte98@gmail.com>
2018-11-06 11:16:07 +0000

    Removed now deprecated h1 placeholder

--- src/pages/stories.php
+++ src/pages/stories.php
@@ -2,7 +2,6 @@
     require_once(realpath( dirname( __FILE__ ) ) . '/../templates/common.php');
     draw_header('Test Page');
 ?>
-    <h1>There will be a navbar here!</h1>
     <button id="refresh_stories">Refresh here!</button>
     <h3>Stories will be inserted below:</h3>
     <div id="stories_container"></div>
