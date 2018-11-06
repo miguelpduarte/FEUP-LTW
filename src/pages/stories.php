9d3d85e25e4a9945c1ef13cc6d081302069fc5b0
miguelpduarte <miguelpduarte98@gmail.com>
2018-11-06 21:55:39 +0000

    Started styling, cute horizontal lines now completed

--- src/pages/stories.php
+++ src/pages/stories.php
@@ -2,10 +2,26 @@
     require_once(realpath( dirname( __FILE__ ) ) . '/../templates/common.php');
     draw_header('Test Page');
 ?>
-    <button id="refresh_stories">Refresh here!</button>
-    <h3>Stories will be inserted below:</h3>
-    <div id="stories_container"></div>
-    <script type="module" src="/js/stories_actions.js"></script>
+    <div id="content">
+        <div id="trending">
+            <div class="line-container">
+                <div class="line"><hr/></div>
+                <div class="line-middle">Trending FireEmoji</div>
+                <div class="line"><hr/></div>
+            </div>
+            <div id="trending_stories_container"></div>
+        </div>
+        <div id="top">
+            <div class="line-container">
+                <div class="line"><hr/></div>
+                <div class="line-middle">Top HatEmoji</div>
+                <div class="line"><hr/></div>
+            </div>
+            <button id="refresh_stories">Refresh here!</button>
+            <div id="stories_container"></div>
+        </div>
+        <script type="module" src="/js/stories_actions.js"></script>
+    </div>
 <?php
     draw_footer();
 ?>
