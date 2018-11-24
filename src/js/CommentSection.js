e016b14a06df55c0506056feaf466da9bb8caff1
Guilherme Silva <guilhermejosesilva@gmail.com>
2018-11-24 13:54:22 +0000

    Added Set to Comment Section

--- src/js/CommentSection.js
+++ src/js/CommentSection.js
@@ -1,95 +1,96 @@
-import {Comment} from './Comment.js';
-import {fetchComments} from './fetch_actions.js';
+import { Comment } from "./Comment.js";
+import { fetchComments } from "./fetch_actions.js";
 
 export class CommentSection {
-  constructor(comments_data, story_id) {
-    this.story_id = story_id;
-    this.n_comments_loaded = comments_data.length;
-    this.comments = comments_data.map(comment => {
-      return new Comment(comment);
-    });
-    this.section = null;
-    this.loading = false;
-  }
-
-  render() {
-    this.section = document.createElement('section');
-    this.section.classList.add('comment-section');
-    this.section.innerHTML = `<h3>Comments</h3>`;
-    for (const comment of this.comments) {
-      this.section.appendChild(comment.render());
+    constructor(comments_data, story_id) {
+        this.story_id = story_id;
+        this.n_comments_loaded = comments_data.length;
+        this.ids = new Set();
+        this.comments = comments_data.map(comment => {
+            this.ids.add(comment.comment_id);
+            return new Comment(comment);
+        });
+        this.section = null;
+        this.loading = false;
     }
 
-    document.addEventListener('scroll', () => this.scrollListener());
-    return this.section;
-  }
+    render() {
+        this.section = document.createElement("section");
+        this.section.classList.add("comment-section");
+        this.section.innerHTML = `<h3>Comments</h3>`;
+        for (const comment of this.comments) {
+            this.section.appendChild(comment.render());
+        }
 
-  scrollListener() {
-    if (this.section === null) return;
+        document.addEventListener("scroll", () => this.scrollListener());
+        return this.section;
+    }
 
-    if (document.body.scrollHeight <=
+    scrollListener() {
+        if (this.section === null) return;
+
+        if (
+            document.body.scrollHeight <=
             document.documentElement.scrollTop + window.innerHeight &&
-        !this.loading) {
-      this.loadMoreComments()
-    }
-  }
-
-  async loadMoreComments(event) {
-    if (this.section === null || this.loading) return;
-
-    this.loading = true;
-
-    // Append loading message
-    let loadingWheel = document.createElement('p');
-    loadingWheel.textContent = 'Loading comments...';
-    this.section.appendChild(loadingWheel);
-
-    // Retrive new comments
-    const comment_data =
-        await fetchComments(this.story_id, 10, this.n_comments_loaded, 2, 0);
-
-
-    this.addComments(comment_data);
-  }
-
-  addComments(comment_data) {
-    // Remove loading message
-    this.section.removeChild(this.section.lastChild);
-    let needFullReload = false;
-    // Append comments
-    if (comment_data == null || comment_data.length == 0) {
-      this.loading = false;
-      return;
+            !this.loading
+        ) {
+            this.loadMoreComments();
+        }
     }
 
-    this.n_comments_loaded += comment_data.length;
-    for (const comment of comment_data) {
-      if(this.comment_loaded(comment))
-          needFullReload = true;
+    async loadMoreComments(event) {
+        if (this.section === null || this.loading) return;
 
-      let comment_object = new Comment(comment);
-      this.comments.push(comment_object);
-      
-      if(!needFullReload)
-        this.section.appendChild(comment_object.render());
+        this.loading = true;
 
+        // Append loading message
+        let loadingWheel = document.createElement("p");
+        loadingWheel.textContent = "Loading comments...";
+        this.section.appendChild(loadingWheel);
+
+        // Retrive new comments
+        const comment_data = await fetchComments(
+            this.story_id,
+            10,
+            this.n_comments_loaded,
+            2,
+            0
+        );
+
+        this.addComments(comment_data);
     }
 
-    if(needFullReload)
-      reloadCommentsFromMemory();
+    addComments(comment_data) {
+        // Remove loading message
+        this.section.removeChild(this.section.lastChild);
+        let needFullReload = false;
+        // Append comments
+        if (comment_data == null || comment_data.length == 0) {
+            this.loading = false;
+            return;
+        }
 
-    this.loading = false;
-  }
+        this.n_comments_loaded += comment_data.length;
+        for (const comment of comment_data) {
+            if (this.comment_loaded(comment)) needFullReload = true;
 
+            this.ids.add(comment.comment_id);
+            let comment_object = new Comment(comment);
+            this.comments.push(comment_object);
 
-  comment_loaded(new_comment) {
-    for (const comment of this.comments) {
-      if(comment.comment_id ===  new_comment.comment_id) {
-        comment.score = new_comment.score;
-        return true;
-      }
+            if (!needFullReload) this.section.appendChild(comment_object.render());
+        }
+
+        if (needFullReload) reloadCommentsFromMemory();
+
+        this.loading = false;
     }
-    return false;
-  }
 
+    comment_loaded(new_comment) {
+        if (this.ids.has(new_comment.comment_id)) {
+            comment.score = new_comment.score;
+            return true;
+        }
+        return false;
+    }
 }
