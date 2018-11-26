622c34d5a62582a579e1db691fe5b3a6bb12286d
Guilherme Silva <guilhermejosesilva@gmail.com>
2018-11-26 18:31:54 +0000

    Syntax highlight

--- src/js/utils.js
+++ src/js/utils.js
@@ -8,7 +8,20 @@ export const getParams = () => {
 
 var mdParser = new Remarkable({
     html: true,
-    linkify: true
+    linkify: true,
+    highlight: function (str, lang) {
+        if (lang && hljs.getLanguage(lang)) {
+          try {
+            return hljs.highlight(lang, str).value;
+          } catch (err) {}
+        }
+    
+        try {
+          return hljs.highlightAuto(str).value;
+        } catch (err) {}
+    
+        return ''; // use external default escaping
+      }
 });
 
 export const mdToHTML = (md) => {
