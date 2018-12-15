47eab77664a1f3a5106864684ecaba507d226a21
miguelpduarte <miguelpduarte98@gmail.com>
2018-12-15 17:42:03 +0000

    Added story patch endpoint to edit content - extensively tested in Postman

--- src/api/story.php
+++ src/api/story.php
@@ -7,14 +7,20 @@
 
     switch ($method) {
         case 'POST':
-          handle_post();
-          break;
+            // Create a story
+            handle_post();
+            break;
         case 'GET':
-          handle_get();
-          break;
+            // Get a preview of all the stories or a specific story, by id
+            handle_get();
+            break;
+        case 'PATCH':
+            // Edit the content of an existing story
+            handle_patch();
+            break;
         default:
-          handle_error();  
-          break;
+            handle_error();  
+            break;
     }
     
     function handle_get() {
@@ -70,9 +76,9 @@
                 'code' => Error("MISSING_PARAM")
                 ]);
                 exit;
-            }
+        }
             
-            if(!isset($data['content']) || $data['content'] === '') {
+        if(!isset($data['content']) || $data['content'] === '') {
                 http_response_code(400);
                 echo json_encode([
                     'success' => false,
@@ -116,7 +122,6 @@
                 'code' => Error("WRONG_CSRF")
                 ]);
             exit;
-
         }
 
         $id = insertStory($currentUser['user_id'], $data['title'], $data['content'], $channel);
@@ -127,6 +132,101 @@
             'story_id' => $id
         ]);
         exit;
+    }
+
+    function handle_patch() {
+        header('Content-Type: application/json');
+        $data = json_decode(file_get_contents('php://input'), true);
+        
+        $currentUser = getLoggedUser();
+
+        if(!$currentUser) {
+            http_response_code(401);
+            echo json_encode([
+                'success' => false,
+                'reason' => "Anonymous user can't edit a Story's content",
+                'code' => Error("UNAUTHORIZED")
+                ]);
+            exit;
+        }
+
+        if (empty($data['new_content'])) {
+            http_response_code(400);
+            echo json_encode([
+                'success' => false,
+                'reason' => 'The New Content field is missing or empty',
+                'code' => Error('MISSING_PARAM')
+            ]);
+            exit;
+        }
+
+        if (empty($data['story_id'])) {
+            http_response_code(400);
+            echo json_encode([
+                'success' => false,
+                'reason' => 'The Story ID field is missing or empty',
+                'code' => Error('MISSING_PARAM')
+            ]);
+            exit;
+        }
+
+        if (empty($data['csrf'])) {
+            http_response_code(401);
+            echo json_encode([
+                'success' => false,
+                'reason' => "CSRF was not provided.",
+                'code' => Error("MISSING_CSRF")
+                ]);
+            exit;
+        }
+
+        if (!verifyCSRF($data['csrf'])) {
+            http_response_code(401);
+            echo json_encode([
+                'success' => false,
+                'reason' => "CSRF did not match. SHOW YOUR ID SIR!",
+                'code' => Error("WRONG_CSRF")
+                ]);
+            exit;
+        }
+
+        try {
+            if (!verifyStoryOwnership($data['story_id'], $currentUser['user_id'])) {
+                http_response_code(401);
+                echo json_encode([
+                    'success' => false,
+                    'reason' => "The story you are trying to change does not belong to you!",
+                    'code' => Error("NOT_OWNER")
+                    ]);
+                exit;
+            }
+        } catch(Exception $e) {
+            http_response_code(400);
+            echo json_encode([
+                'success' => false,
+                'reason' => $e->getMessage(),
+                'code' => Error('OTHER')
+                ]);
+            exit;
+        }
+
+        try {
+            editStoryContent($data['story_id'], $data['new_content']);
+
+            http_response_code(200);
+            echo json_encode([
+                'success' => true,
+            ]);
+            exit;
+        } catch(Exception $e) {
+            http_response_code(500);
+            echo json_encode([
+                'success' => false,
+                'reason' => 'Unknown Error',
+                'code' => Error('OTHER')
+            ]);
+            exit;
+        }
     }
 
     function handle_error() {
