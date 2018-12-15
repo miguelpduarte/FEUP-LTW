b14f3d818787d694f3d106b8f49b072ad2f75281
miguelpduarte <miguelpduarte98@gmail.com>
2018-12-15 16:49:10 +0000

    Improved channel based on story update trigger efficiency

--- src/database/schema.sql
+++ src/database/schema.sql
@@ -156,7 +156,7 @@ END;
 
 DROP TRIGGER IF EXISTS deleteChannelIfItHasNoStories_onupdatestory;
 CREATE TRIGGER IF NOT EXISTS deleteChannelIfItHasNoStories_onupdatestory
-AFTER UPDATE ON stories
+AFTER UPDATE OF channel ON stories
 FOR EACH ROW
 WHEN NOT EXISTS (SELECT * FROM stories WHERE channel = Old.channel)
 BEGIN
