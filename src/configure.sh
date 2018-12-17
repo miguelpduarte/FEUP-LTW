1202e66466e6e1b3a3f0f2dc7aba921b39a37021
miguelpduarte <miguelpduarte98@gmail.com>
2018-12-17 19:17:51 +0000

    Updating script

--- src/configure.sh
+++ src/configure.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-sed -i.bak 's/BASE_DIR_PLACEHOLDER/'$1'/g' ./.htaccess
-sed -i.bak 's/BASE_DIR_PLACEHOLDER/'$1'/g' ./js/components/Navbar.js
-sed -i.bak 's/BASE_DIR_PLACEHOLDER/'$1'/g' ./templates/common.php
+sed -i.bak 's|BASE_DIR_PLACEHOLDER|'$1'|g' ./.htaccess
+sed -i.bak 's|BASE_DIR_PLACEHOLDER|'$1'|g' ./js/components/Navbar.js
+sed -i.bak 's|BASE_DIR_PLACEHOLDER|'$1'|g' ./templates/common.php
